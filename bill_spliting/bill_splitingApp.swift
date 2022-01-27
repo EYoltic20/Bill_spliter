495841920f01392588575aa41816ee42d148a3d9
Yoltic2 <eyolticmg@icloud.com>
2022-01-26 19:51:45 -0600

    Initial Commit

---
+++ bill_spliting/bill_splitingApp.swift
@@ -0,0 +1,17 @@
+//
+//  bill_splitingApp.swift
+//  bill_spliting
+//
+//  Created by Emilio Y Martinez on 26/01/22.
+//
+
+import SwiftUI
+
+@main
+struct bill_splitingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
